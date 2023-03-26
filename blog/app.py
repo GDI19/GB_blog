9cfc3826cabd02616da6687ba558141c7091cea3
GDI <dimon.1982@mail.ru>
2023-03-26 20:01:17 +0700

    add users & articles pages

--- blog/app.py
+++ blog/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from flask import request
 from .views.users import users_app
+from .views.articles import articles_app
 
 
 app = Flask(__name__)
 app.register_blueprint(users_app, url_prefix="/users")
+app.register_blueprint(articles_app, url_prefix='/articles')
 
 
 @app.route('/')
