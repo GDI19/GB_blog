55a4045607ce600bb87ae37de18e49c453605ef6
GDI <dimon.1982@mail.ru>
2023-04-20 18:50:54 +0700

    add rest api swagger

--- blog/configs.py
+++ blog/configs.py
@@ -6,7 +6,14 @@ load_dotenv()
 
 ADMIN_PASSWORD = os.environ['ADMIN_PASSWORD']
 CONFIG_NAME = os.environ["CONFIG_NAME"]
+
 FLASK_ADMIN_SWATCH = 'cosmo'
+
+
+OPENAPI_URL_PREFIX = '/api/swagger'
+OPENAPI_VERSION = '3.0.0'
+OPENAPI_SWAGGER_UI_PATH = '/'
+OPENAPI_SWAGGER_UI_VERSION = '3.51.1'
 
 
 class BaseConfig(object):
