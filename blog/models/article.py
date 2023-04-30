b10548a68316e75534b554950f60804e97b0344a
GDI <dimon.1982@mail.ru>
2023-04-30 21:21:17 +0700

    delete default='' in article author_id

--- blog/models/article.py
+++ blog/models/article.py
@@ -7,7 +7,7 @@ from .article_tag import article_tag_association_table
 
 class Article(db.Model):
     id = Column(Integer, primary_key=True, nullable=False)
-    author_id = Column(Integer, ForeignKey('author.id'), default="", server_default="")
+    author_id = Column(Integer, ForeignKey('author.id'))
     title = Column(String(100), nullable=False) 
     text = Column(Text, nullable=False, default="", server_default="")
     dt_created = Column(DateTime, default=datetime.utcnow, server_default=func.now())
