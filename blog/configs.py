cf66202bce4b63022077289ce2ca5e8671072cd4
GDI <dimon.1982@mail.ru>
2023-04-30 16:29:04 +0700

    correct swagger configurations

--- blog/configs.py
+++ blog/configs.py
@@ -7,21 +7,20 @@ load_dotenv()
 ADMIN_PASSWORD = os.environ['ADMIN_PASSWORD']
 CONFIG_NAME = os.environ["CONFIG_NAME"]
 
-FLASK_ADMIN_SWATCH = 'cosmo'
 
-
-OPENAPI_URL_PREFIX = '/api/swagger'
-OPENAPI_VERSION = '3.0.0'
-OPENAPI_SWAGGER_UI_PATH = '/'
-OPENAPI_SWAGGER_UI_VERSION = '3.22.0'
 
 
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
+    FLASK_ADMIN_SWATCH = 'cosmo'
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
     SECRET_KEY = os.environ["SECRET_KEY"]
+    OPENAPI_URL_PREFIX = '/api/swagger'
+    OPENAPI_VERSION = '3.0.0'
+    OPENAPI_SWAGGER_UI_PATH = '/'
+    OPENAPI_SWAGGER_UI_VERSION = '3.51.1'
     WTF_CSRF_ENABLED = True
 
 
